654c79121665fc6fdb7e4123dbbbf7ff11b4685c
PLR <51248199+plredmond@users.noreply.github.com>
2020-08-28 17:39:49 -0700

    finish sec 1

--- lib/Sec1.hs
+++ lib/Sec1.hs
@@ -57,12 +57,82 @@ i6 = 6
 -- {-@ ignore i6x @-}
 -- {-@ i6x :: { i : Int | i == 6 } @-}
 
--- ** one expression can have many types (not anymore?)
+-- ** one expression can have many types
 
--- {-@ i7 :: { i : Int | i == 6 } @-}
+-- | "all of these work, but we have to choose one"
+{-@ i7 :: { i : Int | i == 6 } @-}
 -- {-@ i7 :: { i : Int | i >= 0 } @-}
 -- {-@ i7 :: { i : Int | i <= 10 } @-}
 -- {-@ i7 :: { i : Int | i >= 0 && i <= 10 } @-}
 -- {-@ i7 :: Int @-}
--- i7 :: Int
--- i7 = 6
+i7 :: Int
+i7 = 6
+
+-- ** refinement types and functions
+
+i8 :: Int
+{-@ i8 :: { i : Int | i == 3 } @-}
+i8 = 1 + 2
+
+plus :: Int -> Int -> Int
+{-@ plus :: a:Int -> b:Int -> { i : Int | i == a + b } @-}
+plus = (+)
+-- this is a *dependent type*
+
+i9 :: Int
+{-@ i9 :: { i : Int | i == 3 } @-}
+i9 = 2 `plus` 1
+
+-- ** preconditions and postconditions
+
+pre :: Int -> Int -> Int
+{-@ pre :: i:Nat -> { j:Int | j >= 2 * i } -> Int @-}
+pre i j = j - i - i
+
+post :: Int -> Int
+{-@ post :: i:Int -> { j:Int | j >= 3*i } @-}
+post i = 3 * i
+
+combine :: Int -> Int
+{-@ combine :: Nat -> Int @-}
+combine i = pre i (post i)
+
+-- ** integer arithmetic
+
+double :: Int -> Int
+{-@ double :: x:Int -> { i:Int | i == 2*x } @-}
+double x = x + x
+
+dist :: Int -> Int -> Int -> Int
+{-@ dist :: x:Int -> y:Int -> z:Int -> {i:Int | i == x*z + y*z } @-}
+dist x y z = (x + y) * z
+
+-- ** exercises
+
+-- | 1 give refined type to abs
+abs :: Int -> Int
+abs x
+    | x < 0 = - x
+    | otherwise = x
+{-@ abs :: x:Int -> { y:Int | y >= 0 } @-}
+
+-- | 2 fix the signature so that ... restricted to suitable nats
+-- {-@ sub :: Nat -> Nat -> Nat @-}
+sub :: Int -> Int -> Int
+sub i j = i - j
+{-@ sub :: i:Nat -> {j:Nat | i>=j} -> Nat @-}
+
+-- | 3 add a sig .. the sum of the two comps of result is equal to the arg
+halve :: Int -> (Int, Int)
+halve i = (j, j + r)
+  where
+    j = i `div` 2
+    r = i `mod` 2
+{-@ halve :: i:Int -> {t:(Int,Int) | i == fst t + snd t} @-}
+
+halve' :: Int -> (Int, Int)
+halve' i = (j, j + r)
+  where
+    j = i `quot` 2
+    r = i `rem` 2
+-- {-@ halve' :: i:Int -> {t:(Int,Int) | i == fst t + snd t} @-}
